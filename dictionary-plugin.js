436ef3a93a44da458dc22830b23b091202cbccb2
Pegah Sarram <pegah.sarram@nasa.gov>
2017-10-17 13:03:10 -0700

    part-b-step-4

--- dictionary-plugin.js
+++ dictionary-plugin.js
@@ -1,8 +1,45 @@
+function getDictionary() {
+    return http.get('/dictionary.json')
+        .then(function (result) {
+            return result.data;
+        });
+}
+
+var objectProvider = {
+    get: function (identifier) {
+        return getDictionary().then(function (dictionary) {
+            if (identifier.key === 'spacecraft') {
+                return {
+                    identifier: identifier,
+                    name: dictionary.name,
+                    type: 'folder',
+                    location: 'ROOT'
+                };
+            } else {
+                var measurement = dictionary.measurements.filter(function (m) {
+                    return m.key === identifier.key;
+                })[0];
+                return {
+                    identifier: identifier,
+                    name: measurement.name,
+                    type: 'example.telemetry',
+                    telemetry: {
+                        values: measurement.values
+                    },
+                    location: 'example.taxonomy:spacecraft'
+                };
+            }
+        });
+    }
+};
+
 function DictionaryPlugin() {
     return function install(openmct) {
         openmct.objects.addRoot({
             namespace: 'example.taxonomy',
             key: 'spacecraft'
         });
+
+        openmct.objects.addProvider('example.taxonomy', objectProvider);
     }
 };
