0e19ccdfc16057d26bd06c4b6a25aa09c2fcf8bf
Joshi <simplyrender@gmail.com>
2019-11-15 10:42:16 -0800

    Part D of tutorial updated

---
+++ historical-telemetry-plugin.js
@@ -0,0 +1,26 @@
+/**
+ * Basic historical telemetry plugin.
+ */
+
+function HistoricalTelemetryPlugin() {
+    return function install (openmct) {
+        var provider = {
+            supportsRequest: function (domainObject) {
+                return domainObject.type === 'example.telemetry';
+            },
+            request: function (domainObject, options) {
+                var url = '/history/' +
+                    domainObject.identifier.key +
+                    '?start=' + options.start +
+                    '&end=' + options.end;
+
+                return http.get(url)
+                    .then(function (resp) {
+                        return resp.data;
+                    });
+            }
+        };
+
+        openmct.telemetry.addProvider(provider);
+    }
+}
